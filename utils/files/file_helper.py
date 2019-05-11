5e283e33ff36dd1edc42221d6853ba3c51691600
Bulhakov Dmytro <mail@dbulgakov.com>
2019-05-12 00:45:57 +0300

    now saving preprocessed data

--- utils/files/file_helper.py
+++ utils/files/file_helper.py
@@ -1,10 +1,20 @@
 import os
 import glob
+import pickle
 
 
 def save_text_file(file_data, file_name, location=''):
     with open('{}/{}'.format(location, file_name), 'w') as f:
         f.write(file_data)
+
+
+def save_binary_file(file_data, file_name, location=''):
+    n_bytes = 2**31
+    max_bytes = 2**31 - 1
+    bytes_out = pickle.dumps(file_data)
+    with open('{}/{}'.format(location, file_name), 'w+b') as f_out:
+        for idx in range(0, n_bytes, max_bytes):
+            f_out.write(bytes_out[idx:idx+max_bytes])
 
 
 def ensure_directory(directory):
