faf3e818512ebb5a18ed5278bd9fe5f61f048c35
Bulhakov Dmytro <mail@dbulgakov.com>
2019-05-12 13:09:52 +0300

    more weather, bugfixes

--- utils/files/file_helper.py
+++ utils/files/file_helper.py
@@ -9,11 +9,10 @@ def save_text_file(file_data, file_name, location=''):
 
 
 def save_binary_file(file_data, file_name, location=''):
-    n_bytes = 2**31
     max_bytes = 2**31 - 1
     bytes_out = pickle.dumps(file_data)
     with open('{}/{}'.format(location, file_name), 'w+b') as f_out:
-        for idx in range(0, n_bytes, max_bytes):
+        for idx in range(0, len(bytes_out), max_bytes):
             f_out.write(bytes_out[idx:idx+max_bytes])
 
 
